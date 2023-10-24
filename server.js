642d0f8860d214c1800edee94bac5464c811eb1a
mustardowl <arianne.dilzer@gmail.com>
2023-10-24 13:32:47 -0400

    arianne

--- server.js
+++ server.js
@@ -8,5 +8,7 @@ app.get('/', (req,res) => {res.send("Hello World")});
 
 app.get('/bam', (req,res) => {res.send("Hello Bam")});
 app.get('/izak', (req,res) => {res.send("Hello Izak")});
+app.get('/eric', (req,res) => {res.send("Hello Eric")});
+app.get('/arianne', (req,res) => {res.send("Hello Arianne")});
 
 app.listen(port, ()=>console.log("Listening"));
